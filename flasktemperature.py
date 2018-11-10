642caa3fbee7ba898337d47ec1329f35bdd6044b
kbutz <kylebutz@gmail.com>
2018-11-10 18:33:50 +0000

    Pushing up old changes from the pi

--- flasktemperature.py
+++ flasktemperature.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def temperature():
-    return read_temp()
+    return "{\"rawTemp\":\"" + read_temp() + "\"}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
