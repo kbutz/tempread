a91134ca8bc8b493073017f9d0fc3530c988360d
Kyle Butz <kylebutz@gmail.com>
2018-11-10 20:21:35 -0600

    Merge branch 'master' of /home/python/PycharmProjects/tempread with conflicts.

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from flask import current_app
+
+from app import db
+
+
+class TemperatureReading(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.TIMESTAMP, default=datetime.utcnow)
+    temp_in_f = db.Column(db.Float())
+
+    def add(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except Exception:
+            current_app.logger.exception("Error adding temp reading row")
+
