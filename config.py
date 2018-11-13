fc8a7fcf8b925d8c6f5a8c2da51b3a930af14017
Kyle Butz <kylebutz@gmail.com>
2018-11-13 16:27:37 -0600

    Moved DS18B20 sensor related code to the same package

--- config.py
+++ config.py
@@ -1,27 +1,6 @@
-import glob
 import os
 
-
-def initialize_modules():
-    # load the kernel modules needed to handle the sensor
-    os.system('modprobe w1-gpio')
-    os.system('modprobe w1-therm')
-
-
-def get_temp_sensor_file_location():
-    try:
-        # From an Adafruit tutorial:
-        # https://learn.adafruit.com/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing/software
-        # base path of a sensor directory that starts with 28, the preface for the ds18b20 serial name
-        base_dir = '/sys/bus/w1/devices/'
-        # glob does unix style pathname pattern matching. If multiple sensors exist, an array of folders would be found
-        device_folder = glob.glob(base_dir + '28*')[0]
-        # The file under the 28-[serial_number] folder where we can find the temperature readings in C*1000
-        return device_folder + '/w1_slave'
-    except Exception as e:
-        # TODO: It would be nice to user the flask logger when logging errors from config
-        print("Could not load temp sensor file location:", e)
-        return ""
+from ds18b20 import initialize_modules, get_temp_sensor_file_location
 
 
 class Config(object):
