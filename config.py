86f4250b989581901ff76bc0758881cfc27edf27
Kyle Butz <kylebutz@gmail.com>
2019-01-26 09:57:47 -0600

    Update config.py with a better local setup example

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     TEMP_SENSOR_FILE_LOCATION = get_temp_sensor_file_location()
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        "mysql+pymysql" + "://" + "root" + ":" + "password" + "@" + "192.168.1.3" + ":" + "3307" + "/" + "tempread"
+        "mysql+pymysql" + "://" + "root" + ":" + "password" + "@" + "localhost" + ":" + "3306" + "/" + "tempread"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
 
