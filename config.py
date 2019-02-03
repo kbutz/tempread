f3394590196cc44aab02b13981dfc917c1ed97ec
Kyle Butz <kylebutz@gmail.com>
2019-02-03 11:51:09 -0600

    Added nohup.out to gitignore, set example config to localhost

--- config.py
+++ config.py
@@ -9,7 +9,9 @@ class Config(object):
     TEMP_SENSOR_FILE_LOCATION = get_temp_sensor_file_location()
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        "mysql+pymysql" + "://" + "root" + ":" + "password" + "@" + "192.168.1.3" + ":" + "3307" + "/" + "tempread"
+        "mysql+pymysql" + "://" + "root" + ":" + "password" + "@" + "localhost" + ":" + "3306" + "/" + "tempread"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
+    CELERY_BROKER_URL = os.environ.get('CELERY_BROKER') or 'amqp://localhost' # use env var or default rabbit mq uri
+    # TODO: optionally add CELERY_BACKEND for celery result consumer
 
